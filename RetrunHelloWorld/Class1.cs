235020c273d1387827255136c86a5240e89661ff
Nikhil Sehgal <csehgan@jci.com>
2016-11-14 22:51:19 +0530

    first commit

---
+++ RetrunHelloWorld/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetrunHelloWorld
+{
+    public static class HelloWord
+    {
+        public static string hello
+        {
+            get
+            {
+                return "Hello World";
+        }
+        }
+    }
+}
